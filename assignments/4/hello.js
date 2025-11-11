c4a44fd61239d9ecb46162c6745dd510cf5ec85b
Ethan Garcia <ethanmgarcia1233@gmail.com>
2025-11-11 14:04:07 -0800

    added and finished hello.js and index test thing

---
+++ assignments/4/hello.js
@@ -0,0 +1,6 @@
+//variable
+const greetingMessage = "JS";
+//function + arrow
+const createMessage = (name) => { return `Hello, ${name}!`; };
+//function call
+console.log(createMessage(greetingMessage));
